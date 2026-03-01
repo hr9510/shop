30a91f5b3fc31df7afc8511069c88d7f88187b0a
hr9510 <himanshurajoriya9510@gmail.com>
2026-03-01 15:28:49 +0530

    Update product fetching to use fetch API

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -28,8 +28,12 @@ export default function Page() {
 
   useEffect(() => {
   const load = async () => {
-    const data = await api("/get_product")
-    setProducts(Array.isArray(data) ? data : []);
+    const data = await fetch("http://localhost:5000/get_product", {
+      method: "GET",
+      credentials: "include"
+    });
+    let res = await data.json();
+    setProducts(Array.isArray(res) ? res : []);
     setLoading(false);
   };
   load();
